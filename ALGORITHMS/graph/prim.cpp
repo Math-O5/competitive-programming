be7656b69ebf8a4497170c4b04cdd38ca571e348
Mathias <mathfernandes@usp.br>
2020-04-01 23:41:52 -0300

    prim and OBI/2008

---
+++ ALGORITHMS/graph/prim.cpp
@@ -0,0 +1,58 @@
+ #include <iostream>
+#include<queue>
+#include<vector>
+#define MAX 10000
+using namespace std;
+
+const int INF = 200000;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // weight and vertex
+vector<int> adj[MAX], w[MAX]; // vertex adjacent to ith
+int n, m, a, b, c, dist[MAX], component[MAX]; // distance from origh
+
+int prim(int s) 
+{
+    for(int i = 0; i < n; ++i)
+        dist[i] = INF;
+    dist[s] = 0;
+    pq.push(make_pair(dist[s], s));
+    while(!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+        component[u] = -1;
+        
+        for(int i = 0; i < adj[u].size(); ++i)
+        {
+            int v = adj[u][i];
+            if(dist[v] > w[u][i] && !component[v]) {
+                dist[v] = w[u][i];
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    int sum = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        sum += dist[i];
+
+    }
+    return sum;
+}
+
+int main()
+{
+    // numbers vertex and conections
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i)
+    {
+        cin >> a >> b >> c;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        w[a].push_back(c);
+        w[b].push_back(c);
+    }
+    cout << prim(0) << '\n';
+
+    return 0;
+}
