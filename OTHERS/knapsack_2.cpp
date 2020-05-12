b5f58a36053595f26772217bfe49f760cbd8ea13
Mathias <mathfernandes@usp.br>
2020-05-11 23:44:10 -0300

    knapsack

--- OTHERS/knapsack_2.cpp
+++ OTHERS/knapsack_2.cpp
@@ -8,17 +8,25 @@ using namespace std;
 
 const int INF = 0x3f3f3f3f;
 vector<int> weights(101, INF), values(101, INF); 
+vector<int> dp (1e5+1, INF);
+int n, w, sum;
+
+// int knapsack2_recursivo(int value, int i) {
+//     int nao_pega; 
+//     int pega;
+
+//     if(n -1 == i) {
+//         nao_pega = INF;
+//         pega = weights[i];
+//     } else {
+//         nao_pega = knapsack2_recursivo(value, i + 1);
+//         pega = weights[i] + knapsack2_recursivo(value + values[i], i + 1);
+//     }
+
+//     return dp[value] = min(pega, nao_pega);
+// }
 
 void knapsack2() {
-    int n, w, sum = 0;
-    cin >> n >> w;
-
-    vector<int> dp (1e5+1, INF);
-
-    for(int i = 0; i < n; ++i) {
-        cin >> weights[i] >> values[i];
-        sum += values[i];
-    }
 
     dp[0] = 0;
     for(int i = 0; i < n; ++i) {
@@ -26,18 +34,28 @@ void knapsack2() {
             dp[j + values[i]] = min(dp[j + values[i]], weights[i] + dp[j]);
         }
     }
-
-    int i = sum;
-    while(i >= 0 && dp[i] > w) {
-        --i;
-    }
-
-    cout << (i >= 0)? i : 0 << '\n';
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+  
+    cin >> n >> w;
+    for(int i = 0; i < n; ++i) {
+        cin >> weights[i] >> values[i];
+        sum += values[i];
+    }
+
     knapsack2();
+    //knapsack_recursivo(0, 0);
+
+    // imprimir resposta
+    int i = sum;
+    while(i >= 0 && (dp[i] > w)) {
+        --i;
+    }
+
+    cout << ((i >= 0)? i : 0) << '\n';
+
     return 0;
 }
